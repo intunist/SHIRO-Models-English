9061c79e10fee3f512e61ff89ff65e6936deb252
heteric <110702124+hetteric@users.noreply.github.com>
2023-02-04 16:18:05 -0500

    Create install-shiro-ubuntu.sh

---
+++ install-shiro-ubuntu.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get -y install git build-essential lua5.1
+git clone https://github.com/Sleepwalking/SHIRO
+git clone https://github.com/Sleepwalking/ciglet
+cd ciglet
+make single-file
+mv single-file ../SHIRO/external/ciglet
+cd ..
+cd SHIRO/external
+git clone https://github.com/Sleepwalking/liblrhsmm
+cd liblrhsmm
+make
+cd ../..
+mkdir build
+make
+cd ..
+rm -rf ciglet
